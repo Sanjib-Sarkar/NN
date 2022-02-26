12acd5b23448325e02f5d2248529d290b6ad2d33
SanjibSarkarU <95769811+SanjibSarkarU@users.noreply.github.com>
2022-02-26 12:43:12 -0600

    edited

--- test/junk.py
+++ test/junk.py
@@ -81,8 +81,93 @@ series += noise
 
 plt.figure(figsize=(10, 6))
 plot_series(time, series)
-plt.show()
+# plt.show()
 
 """ll right, this looks realistic enough for now. Let's try to forecast it.
  We will split it into two periods: the training period and the validation period 
  (in many cases, you would also want to have a test period). The split will be at time step 1000."""
+split_time = 1000
+time_train = time[:split_time]
+x_train = series[:split_time]
+time_valid = time[split_time:]
+x_valid = series[split_time:]
+
+
+def autocorrelation(time, amplitude, seed=None):
+    rnd = np.random.RandomState(seed)
+    phi1 = 0.5
+    phi2 = -0.1
+    ar = rnd.randn(len(time) + 50)
+    ar[:50] = 100
+    for step in range(50, len(time) + 50):
+        ar[step] += phi1 * ar[step - 50]
+        ar[step] += phi2 * ar[step - 33]
+    return ar[50:] * amplitude
+
+
+# def autocorrelation(time, amplitude, seed=None):
+#     rnd = np.random.RandomState(seed)
+#     phi = 0.8
+#     ar = rnd.randn(len(time) + 1)
+#     for step in range(1, len(time) + 1):
+#         ar[step] += phi * ar[step - 1]
+#     return ar[1:] * amplitude
+
+series = autocorrelation(time, 10, seed=42)
+plot_series(time[:200], series[:200])
+# plt.show()
+
+series = autocorrelation(time, 10, seed=42) + trend(time, 2)
+plot_series(time[:200], series[:200])
+# plt.show()
+
+series = autocorrelation(time, 10, seed=42) + seasonality(time, period=50, amplitude=150) + trend(time, 2)
+plot_series(time[:200], series[:200])
+# plt.show()
+
+series = autocorrelation(time, 10, seed=42) + seasonality(time, period=50, amplitude=150) + trend(time, 2)
+series2 = autocorrelation(time, 5, seed=42) + seasonality(time, period=50, amplitude=2) + trend(time, -1) + 550
+series[200:] = series2[200:]
+#series += noise(time, 30)
+plot_series(time[:300], series[:300])
+# plt.show()
+
+
+def impulses(time, num_impulses, amplitude=1, seed=None):
+    rnd = np.random.RandomState(seed)
+    impulse_indices = rnd.randint(len(time), size=10)
+    series = np.zeros(len(time))
+    for index in impulse_indices:
+        series[index] += rnd.rand() * amplitude
+    return series
+
+
+series = impulses(time, 10, seed=42)
+plot_series(time, series)
+# plt.show()
+
+
+def autocorrelation(source, φs):
+    ar = source.copy()
+    max_lag = len(φs)
+    for step, value in enumerate(source):
+        for lag, φ in φs.items():
+            if step - lag > 0:
+              ar[step] += φ * ar[step - lag]
+    return ar
+
+signal = impulses(time, 10, seed=42)
+series = autocorrelation(signal, {1: 0.99})
+plot_series(time, series)
+plt.plot(time, signal, "k-")
+# plt.show()
+
+signal = impulses(time, 10, seed=42)
+series = autocorrelation(signal, {1: 0.70, 50: 0.2})
+plot_series(time, series)
+plt.plot(time, signal, "k-")
+# plt.show()
+
+series_diff1 = series[1:] - series[:-1]
+plot_series(time[1:], series_diff1)
+plt.show()
