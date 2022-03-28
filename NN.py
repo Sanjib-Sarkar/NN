02835dada8972ed94610bc3197ea4c4edf81e451
SanjibSarkarU <95769811+SanjibSarkarU@users.noreply.github.com>
2022-03-28 16:40:28 -0500

    edited:iverloop

--- NN.py
+++ NN.py
@@ -1,9 +1,13 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from tensorflow import keras
+# from tensorflow import keras
 import tensorflow as tf
+from keras import optimizers
 
+# from tensorflow.python.keras import optimizers
+
+fig, ax = plt.subplots(1, 1, figsize=(12, 5), tight_layout=True, dpi=100)
 file = 'Data/20220222_153859_hlc.csv'
 df = pd.read_csv(file, skiprows=1)
 df = df.rename(columns={"Latitude (Deg N)": 'lat', "Longitude (Deg W)": 'lng'}, errors="raise")
@@ -11,7 +15,7 @@ df = df.rename(columns={"Latitude (Deg N)": 'lat', "Longitude (Deg W)": 'lng'}, 
 # print(df.head(5))
 df = df.loc[(df['lat'] != 0)]
 # print(len(df.lat))
-start = 11400
+start = 8500
 end = 13700
 series_time = df.Time[start:end]
 series_lat = df.lat[start:end]
@@ -98,7 +102,7 @@ def windowed_dataset(series, window_size, batch_size, shuffle_buffer):
 
 
 # training and validation
-split_time = 800
+split_time = 3900
 time_train = series_time[:split_time]
 x_train_lat = series_lat[:split_time]
 x_train_lng = series_lng[:split_time]
@@ -106,39 +110,100 @@ x_train_lng = series_lng[:split_time]
 time_valid = series_time[split_time:]
 x_valid_lat = series_lat[split_time:]
 x_valid_lng = series_lng[split_time:]
+print(f'total: {len(series_lat)}, x_train_lat:{len(x_train_lat)}, x_valid_lat:{len(x_valid_lat)}')
 
-window_size = 20
+window_size = 15
 batch_size = 32
-shuffle_buffer_size = 1000
+shuffle_buffer_size = 100
 
 dataset_lat = windowed_dataset(x_train_lat, window_size=window_size, batch_size=batch_size,
                                shuffle_buffer=shuffle_buffer_size)
 
 dataset_lng = windowed_dataset(x_train_lng, window_size=window_size, batch_size=batch_size,
                                shuffle_buffer=shuffle_buffer_size)
+# print(dataset_lng)
+optimisers = [tf.keras.optimizers.SGD]
+# tf.keras.optimizers.SGD, tf.keras.optimizers.Adam, tf.keras.optimizers.RMSprop,
+# tf.keras.optimizers.Adadelta, tf.keras.optimizers.Adagrad, tf.keras.optimizers.Adamax,
+# tf.keras.optimizers.Nadam, tf.keras.optimizers.Ftrl]
+labels = ['SGD']
 
-l0 = tf.keras.layers.Dense(1, input_shape=[window_size])
-model = tf.keras.models.Sequential([l0])
+# l0 = tf.keras.layers.Dense(1, input_shape=[window_size])
+model = tf.keras.models.Sequential([tf.keras.layers.Dense(10,  input_shape=[window_size],  activation="relu"),
+                                    tf.keras.layers.Dense(10, activation="relu"),
+                                    tf.keras.layers.Dense(1)])
+model1 = model
 
-model.compile(loss="mse", optimizer=tf.keras.optimizers.SGD(learning_rate=1e-6, momentum=0.9))
-model.fit(dataset_lat, epochs=120, verbose=0)
+# model.compile(loss="mse", optimizer=tf.keras.optimizers.SGD(learning_rate=1e-5, momentum=0.9))
+# model.compile(loss="mse", optimizer=tf.keras.optimizers.Adam(learning_rate=1e-5))
+# model.fit(dataset_lat, epochs=120, verbose=0)
 
-# print("Layer weights{}".format(l0.get_weights()))
+lr = 1e-6
+epochs = 100
+# lr_schedule = tf.keras.callbacks.LearningRateScheduler(lambda epoch: lr * 10 ** (epoch / 20))
+for optimiser, label in zip(optimisers, labels):
+    model.compile(loss="mse", optimizer=optimiser(learning_rate=lr, momentum=0.9))
+    model1.compile(loss="mse", optimizer=optimiser(learning_rate=lr, momentum=0.9))
 
-# print(series_lat[1:21])
-# print(model.predict(series_lat[1:21][np.newaxis]))
+    model.fit(dataset_lat, epochs=epochs, verbose=0)
 
-forecast = []
-for time in range(len(series_lat) - window_size):
-    forecast.append(model.predict(series_lat[time:time + window_size][np.newaxis]))
+    # plt.plot(history.history['loss'], label='train')
+    # plt.legend()
+    # plt.show()
 
-forecast = forecast[split_time - window_size:]
-results = np.array(forecast)[:, 0, 0]
+    forecast_lat = []
+    for time in range(len(series_lat) - window_size):
+        forecast_lat.append(model.predict(series_lat[time:time + window_size][np.newaxis]))
+    forecast_lat = forecast_lat[split_time - window_size:]
+    results_lat = np.array(forecast_lat)[:, 0, 0]
 
-fig, ax = plt.subplots(1, 1, figsize=(12, 5))
+    model1.fit(dataset_lng, epochs=epochs, verbose=0)
 
-ax.plot(x_valid_lat)
-ax.plot(results)
+    # Prediction
+    forcast_lng = []
+    for time in range(len(series_lng) - window_size):
+        forcast_lng.append(model1.predict(series_lng[time:time + window_size][np.newaxis]))
+    forcast_lng = forcast_lng[split_time - window_size:]
+    results_lng = np.array(forcast_lng)[:, 0, 0]
+
+    # Plot prediction
+    ax.plot(forcast_lng, forecast_lat, label=label)
+
+    # # plot learning rates vs loss ; Add 'callbacks=[lr_schedule],' inside model.fit
+    # lrs = lr * (10 ** (np.arange(epochs) / 20))
+    # ax.semilogx(lrs, history.history['loss'])
+    # plt.axis([lr, 1e-1, 0, 250])
+    # plt.xlabel('Learning Rate')
+    # plt.ylabel('Loss')
+    # plt.show()
+
+    # # # plot epochs vs loss
+    # loss = history.history['loss']
+    # epochs = range(3, len(loss))
+    # ax.plot(epochs, loss[3:], label='Training Loss')
+    # plt.xlabel('Epochs')
+    # plt.ylabel('Error')
+    # plt.show()
+
+    # print("Layer weights{}".format(l0.get_weights()))
+
+    # print(series_lat[1:21])
+    # print(model.predict(series_lat[1:21][np.newaxis]))
+
+    # forecast = []
+    # for time in range(len(series_lat) - window_size):
+    #     forecast.append(model.predict(series_lat[time:time + window_size][np.newaxis]))
+    #
+    # forecast = forecast[split_time - window_size:]
+    # results = np.array(forecast)[:, 0, 0]
+
+
+# print("MSE: ", tf.keras.metrics.mean_absolute_error(x_valid_lat, results).numpy())
+# ax.plot(x_valid_lat)
+# ax.plot(results)
+# plt.scatter(series_time, results)
 # ax.set_xticks(ax.get_xticks()[::180])
 
+ax.plot(x_valid_lng, x_valid_lat, label='Latitude')
+plt.legend()
 plt.show()
--- lstm.py
+++ lstm.py
@@ -3,30 +3,40 @@ import pandas as pd
 import tensorflow as tf
 from keras.preprocessing.sequence import TimeseriesGenerator
 from matplotlib import pyplot as plt
+import pymap3d as pm
 
 # from sklearn.preprocessing import MinMaxScaler
 # from sklearn.metrics import mean_squared_error
 
 plt.style.use('dark_background')
 
-fig, ax = plt.subplots(1, 1, figsize=(7, 5), tight_layout=True, dpi=100)
 file = 'Data/20220222_153859_hlc.csv'
 df = pd.read_csv(file, skiprows=1)
 df = df.rename(columns={"Latitude (Deg N)": 'lat', "Longitude (Deg W)": 'lng'}, errors="raise")
 df = df.loc[(df['lat'] != 0)]
+df['h'] = 0
+
+' Convert to local coordinate system'
+df['e_lat'], df['n_lng'], df['u'] = pm.geodetic2enu(df.lat, df.lng, df.h, df.lat[len(df.lat) - 1],
+                                                    df.lng[len(df.lng) - 1], df.h, ell=None, deg=True)
 
 start = 10500
 end = 13700
-series_time = df.Time[start:end].to_numpy()
-series_lat = df.lat[start:end].to_numpy()
-series_lng = df.lng[start:end].to_numpy()
+series_time = df.Time[start:end].to_list
+series_lat = df.e_lat[start:end]
+series_lng = df.n_lng[start:end]
+
+coordinates = [[i, j] for i, j in zip(series_lat, series_lng)]
+coordinates = np.array(coordinates)
 
 
-def train_test_split(dataset, percentage):
-    train_size = int(len(dataset) * percentage)
-    # test_size = len(dataset) - train_size
-    train, test = dataset[0:train_size, :], dataset[train_size:len(dataset), :]
-    return train, test
+# print(coordinates[:10])
+
+def ts_data_preparation(features, window=5, future=1, batch=1, shuffle=False):
+    target = features[(future - 1):]
+    features = features[:-(future - 1)] if future != 1 else features
+    tensor = TimeseriesGenerator(features, target, length=window, batch_size=batch, shuffle=shuffle)
+    return tensor
 
 
 def test_data_preparation(series, window_size, batch_size):
@@ -38,45 +48,30 @@ def test_data_preparation(series, window_size, batch_size):
     return dataset
 
 
-def ts_data_preparation(features, window=5, future=1, batch=1, shuffle=False):
-    target = features[(future - 1):]
-    features = features[:-(future - 1)] if future != 1 else features
-    tensor = TimeseriesGenerator(features, target, length=window, batch_size=batch, shuffle=shuffle)
-    return tensor
+def train_test_split(dataset, percentage):
+    train_size = int(len(dataset) * percentage)
+    # test_size = len(dataset) - train_size
+    train, test = dataset[0:train_size, :], dataset[train_size:len(dataset), :]
+    return train, test
 
 
-# reshape
-series_lat = np.reshape(series_lat, (len(series_lat), 1))
-series_lng = np.reshape(series_lng, (len(series_lng), 1))
+train_coor, test_coor = train_test_split(coordinates, 0.7)
 
-# split into train and test sets
-train_lat, test_lat = train_test_split(series_lat, 0.7)
-train_lng, test_lng = train_test_split(series_lng, 0.7)
-
-tl = test_lat
-tln = test_lng
-
-window_size = 10
-future = 2
+window_size = 15
+future = 1
 batch_size = 32
 
-train_lat = ts_data_preparation(train_lat, window=window_size, future=future, batch=batch_size,
-                                shuffle=True)
-train_lng = ts_data_preparation(train_lng, window=window_size, future=future, batch=batch_size,
-                                shuffle=True)
-
-test_lat = test_data_preparation(test_lat, window_size=window_size, batch_size=batch_size)
-test_lng = test_data_preparation(test_lng, window_size=window_size, batch_size=batch_size)
+train_coor = ts_data_preparation(train_coor, window=window_size, future=future, batch=batch_size)
 
 
 def model_lstm(w_size):
-    units = 50
+    units = 64
     tf.keras.backend.clear_session()
     model = tf.keras.models.Sequential([
-        tf.keras.layers.LSTM(units, activation='relu', return_sequences=True, input_shape=(w_size, 1)),
-        tf.keras.layers.LSTM(units, activation='relu'),
-        tf.keras.layers.Dense(1, activation='linear')])
-    model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=1e-7, momentum=0.9),
+        tf.keras.layers.LSTM(units, activation='relu', return_sequences=True, input_shape=(w_size, 2)),
+        tf.keras.layers.LSTM(units),
+        tf.keras.layers.Dense(2, activation='linear')])
+    model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.001, momentum=0.9),
                   loss='mse')
     model.summary()
     # print('Train...', model.summary())
@@ -87,16 +82,98 @@ model = model_lstm(window_size)
 
 epochs = 50
 
-model.fit(train_lat, epochs=epochs, verbose=1)
-testPredict_lat = model.predict(test_lat)
+model.fit(train_coor, epochs=epochs, verbose=1)
 
-model_ln = model_lstm(window_size)
-model_ln.fit(train_lng, epochs=epochs, verbose=1)
-testPredict_lng = model_ln.predict(test_lng)
 
-plt.plot(testPredict_lng, testPredict_lat, color='y', label=f'P:future:{future}')
-plt.plot(tln, tl, color='r', label='Original')
 
-plt.title(f'WindowSize: {window_size}')
-plt.legend()
-plt.show()
+# fig, ax = plt.subplots(1, 1, figsize=(7, 5), tight_layout=True, dpi=100)
+# file = 'Data/20220222_153859_hlc.csv'
+# df = pd.read_csv(file, skiprows=1)
+# df = df.rename(columns={"Latitude (Deg N)": 'lat', "Longitude (Deg W)": 'lng'}, errors="raise")
+# df = df.loc[(df['lat'] != 0)]
+#
+# start = 10500
+# end = 13700
+# series_time = df.Time[start:end].to_numpy()
+# series_lat = df.lat[start:end].to_numpy()
+# series_lng = df.lng[start:end].to_numpy()
+#
+#
+# def train_test_split(dataset, percentage):
+#     train_size = int(len(dataset) * percentage)
+#     # test_size = len(dataset) - train_size
+#     train, test = dataset[0:train_size, :], dataset[train_size:len(dataset), :]
+#     return train, test
+#
+#
+# def test_data_preparation(series, window_size, batch_size):
+#     dataset = tf.data.Dataset.from_tensor_slices(series)
+#     dataset = dataset.window(window_size, shift=1, drop_remainder=True)
+#     dataset = dataset.flat_map(lambda window: window.batch(window_size))
+#     # dataset = dataset.shuffle(shuffle_buffer).map(lambda window: (window[:-1], window[-1:]))
+#     dataset = dataset.batch(batch_size).prefetch(1)
+#     return dataset
+#
+#
+# def ts_data_preparation(features, window=5, future=1, batch=1, shuffle=False):
+#     target = features[(future - 1):]
+#     features = features[:-(future - 1)] if future != 1 else features
+#     tensor = TimeseriesGenerator(features, target, length=window, batch_size=batch, shuffle=shuffle)
+#     return tensor
+#
+#
+# # reshape
+# series_lat = np.reshape(series_lat, (len(series_lat), 1))
+# series_lng = np.reshape(series_lng, (len(series_lng), 1))
+#
+# # split into train and test sets
+# train_lat, test_lat = train_test_split(series_lat, 0.7)
+# train_lng, test_lng = train_test_split(series_lng, 0.7)
+#
+# tl = test_lat
+# tln = test_lng
+#
+# window_size = 10
+# future = 2
+# batch_size = 32
+#
+# train_lat = ts_data_preparation(train_lat, window=window_size, future=future, batch=batch_size,
+#                                 shuffle=True)
+# train_lng = ts_data_preparation(train_lng, window=window_size, future=future, batch=batch_size,
+#                                 shuffle=True)
+#
+# test_lat = test_data_preparation(test_lat, window_size=window_size, batch_size=batch_size)
+# test_lng = test_data_preparation(test_lng, window_size=window_size, batch_size=batch_size)
+#
+#
+# def model_lstm(w_size):
+#     units = 50
+#     tf.keras.backend.clear_session()
+#     model = tf.keras.models.Sequential([
+#         tf.keras.layers.LSTM(units, activation='relu', return_sequences=True, input_shape=(w_size, 1)),
+#         tf.keras.layers.LSTM(units, activation='relu'),
+#         tf.keras.layers.Dense(1, activation='linear')])
+#     model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=1e-7, momentum=0.9),
+#                   loss='mse')
+#     model.summary()
+#     # print('Train...', model.summary())
+#     return model
+#
+#
+# model = model_lstm(window_size)
+#
+# epochs = 50
+#
+# model.fit(train_lat, epochs=epochs, verbose=1)
+# testPredict_lat = model.predict(test_lat)
+#
+# model_ln = model_lstm(window_size)
+# model_ln.fit(train_lng, epochs=epochs, verbose=1)
+# testPredict_lng = model_ln.predict(test_lng)
+#
+# plt.plot(testPredict_lng, testPredict_lat, color='y', label=f'P:future:{future}')
+# plt.plot(tln, tl, color='r', label='Original')
+#
+# plt.title(f'WindowSize: {window_size}')
+# plt.legend()
+# plt.show()
