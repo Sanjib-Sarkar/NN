f6fb47fe6b30ab6172993b1bb8ec943bc4ef3c41
SanjibSarkarU <95769811+SanjibSarkarU@users.noreply.github.com>
2022-03-30 19:20:09 -0500

    learningRate callback

--- lstm.py
+++ lstm.py
@@ -4,11 +4,14 @@ import tensorflow as tf
 from keras.preprocessing.sequence import TimeseriesGenerator
 from matplotlib import pyplot as plt
 import pymap3d as pm
+import matplotlib.colors as pltc
 
 # from sklearn.preprocessing import MinMaxScaler
 # from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import StandardScaler
 
 plt.style.use('dark_background')
+colors = [k for k, v in pltc.cnames.items()]
 
 file = 'Data/20220222_153859_hlc.csv'
 df = pd.read_csv(file, skiprows=1)
@@ -16,12 +19,17 @@ df = df.rename(columns={"Latitude (Deg N)": 'lat', "Longitude (Deg W)": 'lng'}, 
 df = df.loc[(df['lat'] != 0)]
 df['h'] = 0
 
+index = int(len(df.lat) / 2)
+
 ' Convert to local coordinate system'
-df['e_lat'], df['n_lng'], df['u'] = pm.geodetic2enu(df.lat, df.lng, df.h, df.lat[len(df.lat) - 1],
-                                                    df.lng[len(df.lng) - 1], df.h, ell=None, deg=True)
+df['e_lat'], df['n_lng'], df['u'] = pm.geodetic2enu(df.lat, df.lng, df.h, df.lat[index],
+                                                    df.lng[index], df.h, ell=None, deg=True)
 
 start = 10500
 end = 13700
+lat = df.lat[start:end]
+lng = df.lng[start:end]
+
 series_time = df.Time[start:end].to_list
 series_lat = df.e_lat[start:end]
 series_lng = df.n_lng[start:end]
@@ -32,22 +40,6 @@ coordinates = np.array(coordinates)
 
 # print(coordinates[:10])
 
-def ts_data_preparation(features, window=5, future=1, batch=1, shuffle=False):
-    target = features[(future - 1):]
-    features = features[:-(future - 1)] if future != 1 else features
-    tensor = TimeseriesGenerator(features, target, length=window, batch_size=batch, shuffle=shuffle)
-    return tensor
-
-
-def test_data_preparation(series, window_size, batch_size):
-    dataset = tf.data.Dataset.from_tensor_slices(series)
-    dataset = dataset.window(window_size, shift=1, drop_remainder=True)
-    dataset = dataset.flat_map(lambda window: window.batch(window_size))
-    # dataset = dataset.shuffle(shuffle_buffer).map(lambda window: (window[:-1], window[-1:]))
-    dataset = dataset.batch(batch_size).prefetch(1)
-    return dataset
-
-
 def train_test_split(dataset, percentage):
     train_size = int(len(dataset) * percentage)
     # test_size = len(dataset) - train_size
@@ -55,37 +47,102 @@ def train_test_split(dataset, percentage):
     return train, test
 
 
-train_coor, test_coor = train_test_split(coordinates, 0.7)
+def ts_from_array(data, target, window, future, batch, shuffle):
+    data = data[:-(window + 1)] if future != 1 else data
+    targets = target
+    print("length: ", len(data), len(targets) if targets is not None else '')
+    tensor = tf.keras.preprocessing.timeseries_dataset_from_array(data, targets, sequence_length=window,
+                                                                  sequence_stride=1,
+                                                                  sampling_rate=1, batch_size=batch, shuffle=shuffle,
+                                                                  seed=None,
+                                                                  start_index=None, end_index=None)
+    return tensor
+
+
+# create scaler
+scaler = StandardScaler()
+# fit scaler on data
+scaler.fit(coordinates)
+# apply transform
+coordinates = scaler.transform(coordinates)
+# print(coordinates[:17])
+
+train, test = train_test_split(coordinates, 0.7)
 
 window_size = 15
-future = 1
-batch_size = 32
+future = 2
+batch_size = 128
 
-train_coor = ts_data_preparation(train_coor, window=window_size, future=future, batch=batch_size)
+target = train[(window_size + future) - 1:]
 
+print(f'train: {len(train)}, target: {len(target)}')
+
+xy_train = ts_from_array(train, target, window=window_size, future=future, batch=batch_size, shuffle=True)
+
+
+# xy_train = tf.convert_to_tensor(xy_train,dtype=tf.float64)
+
+
+# xy_test = ts_from_array(test, None, window=window_size, future=future, batch=batch_size, shuffle=False)
+
+# print('xy_train:', list(xy_train.as_numpy_iterator())[:2])
 
 def model_lstm(w_size):
-    units = 64
+    units = 16
     tf.keras.backend.clear_session()
     model = tf.keras.models.Sequential([
-        tf.keras.layers.LSTM(units, activation='relu', return_sequences=True, input_shape=(w_size, 2)),
+        tf.keras.layers.LSTM(32, activation='relu', return_sequences=True, input_shape=(w_size, 2)),
+        tf.keras.layers.LSTM(units, activation='relu', return_sequences=True),
+        tf.keras.layers.LSTM(units, activation='relu', return_sequences=True),
         tf.keras.layers.LSTM(units),
         tf.keras.layers.Dense(2, activation='linear')])
-    model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.001, momentum=0.9),
-                  loss='mse')
     model.summary()
     # print('Train...', model.summary())
     return model
 
 
-model = model_lstm(window_size)
+# lr = 2e-4
+# lr_schedular = tf.keras.callbacks.LearningRateScheduler(lambda epoch: lr * 10 ** (epoch / 30))
+# model = model_lstm(window_size)
+# model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=lr, momentum=0.9), loss='mse', metrics=['mae'])
+# epochs = 100
 
-epochs = 50
+# history = model.fit(xy_train, epochs=epochs, verbose=1, callbacks=[lr_schedular])
+# plt.semilogx(history.history["lr"], history.history["loss"], label='loss')
+# plt.axis([1e-3, 1, 0, 0.4])
+# plt.xlabel('Learning rate')
+# plt.legend()
+# plt.show()
 
-model.fit(train_coor, epochs=epochs, verbose=1)
 
 
 
+# print(history.history.keys())
+# # dict_keys(['loss', 'accuracy'])
+
+# plt.plot(history.history['loss'], label='Loss')
+# plt.plot(history.history['accuracy'], label='Accuracy')
+
+# xy_test = ts_from_array(test, None, window=window_size, future=future, batch=batch_size, shuffle=False)
+#
+# xy_predict = model.predict(xy_test)
+#
+# predicted_coor = scaler.inverse_transform(xy_predict)
+#
+# df_n = pd.DataFrame(predicted_coor, columns=('p_lat', 'p_lng'))
+# df_n['h'] = 0
+# ' Back to lag lng'
+# df_n['Predted_lat'], df_n['Predicted_lng'], _ = pm.enu2geodetic(df_n.p_lat, df_n.p_lng, df_n.h,
+#                                                                 df.lat[index], df.lng[index],
+#                                                                 df.h[index], ell=None, deg=True)
+#
+# plt.plot(df_n.Predicted_lng, df_n.Predted_lat, color='r', label=f'predicted, future:{future}')
+# plt.plot(lng, lat, label='Original')
+# plt.title(f'Window{window_size}')
+# plt.legend()
+# plt.show()
+
+###  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.
 # fig, ax = plt.subplots(1, 1, figsize=(7, 5), tight_layout=True, dpi=100)
 # file = 'Data/20220222_153859_hlc.csv'
 # df = pd.read_csv(file, skiprows=1)
